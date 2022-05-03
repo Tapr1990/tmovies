90e836c71556493ff9a2c4bb44733e01949b4e97
Tiago Rufino <trufino90@gmail.com>
2022-05-03 22:57:46 +0100

    Implementado dois Slides na Homepage e estlização css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 import Hero from './Hero'
+import Scrollslide from './Scrollslide'
 
 
 
 export default function Home() {
 
-
   return (
       <main>
-   
         <Hero />
-      
+        <Scrollslide />
       </main>
+  );
+}
+
+   
 
 
         
-  )
-}
