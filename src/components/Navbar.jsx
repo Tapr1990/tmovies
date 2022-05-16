fa5deae8233934eebff097591de5b3f82b55171c
Tiago Rufino <trufino90@gmail.com>
2022-05-16 22:58:17 +0100

    Criação do footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,34 @@ import Header from './Header';
 
 
 export default function Navbar() {
+
+  const menuHamburguer = () => {
+
+    let menu = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".nav_container");
+
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  }
+
+
+
   return (
-    <header className="header_container shadow">
+    <header className="header_container">
       <Header />
-      <nav className="nav_container">
+      <nav className="nav_container" onClick={menuHamburguer}>
           <ul className="nav_list">
               <li className="nav_item"><NavLink className="nav_link" to="/">Home</NavLink></li>
               <li className="nav_item"><NavLink className="nav_link" to="/movies">Movies</NavLink></li>
               <li className="nav_item"><NavLink className="nav_link" to="/favourites">Favourites</NavLink></li>
               <li className="nav_item"><NavLink className="nav_link" to="/contacts">Contacts</NavLink></li>
           </ul>
-          <div className='container-log'>
-            <span className="sign-in">Sign In</span>
-            <span className="log-in">Log In</span>
-          </div>
       </nav>
+      <div className='container-log'>
+        <span className="sign-in">Sign In</span>
+        <span className="log-in">Log In</span>
+      </div>
+      <div className="bx bx-menu" id="menu-icon" onClick={menuHamburguer}/>
     </header>
       
               
